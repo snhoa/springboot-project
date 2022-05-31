45c6dee4ae794ff9421fcc39f4c13ac3f1d05e46
snhoa <snhoa16@naver.com>
2022-06-01 03:50:46 +0900

    게시판 생성

---
+++ src/main/java/com/example/board/controller/BoardController.java
@@ -0,0 +1,73 @@
+package com.example.board.controller;
+
+import com.example.board.dto.BoardDto;
+import com.example.board.service.BoardService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@AllArgsConstructor
+public class BoardController {
+    private BoardService boardService;
+
+    /* 게시글 목록 */
+    @GetMapping("/")
+    public String list(Model model) {
+        List<BoardDto> boardList = boardService.getBoardlist();
+
+        model.addAttribute("boardList", boardList);
+        return "board/list.html";
+    }
+
+//    @GetMapping("/")
+//    public String list() {
+//        return "board/list.html";
+//    }
+//
+
+    @GetMapping("/post/{no}")
+    public String detail(@PathVariable("no") Long no, Model model) {
+        BoardDto boardDto = boardService.getPost(no);
+
+        model.addAttribute("boardDto", boardDto);
+        return "board/detail.html";
+    }
+
+    @GetMapping("/post/edit/{no}")
+    public String edit(@PathVariable("no") Long no, Model model) {
+        BoardDto boardDTO = boardService.getPost(no);
+
+        model.addAttribute("boardDto", boardDTO);
+        return "board/update.html";
+    }
+
+    @PutMapping("/post/edit/{no}")
+    public String update(BoardDto boardDTO) {
+        boardService.savePost(boardDTO);
+
+        return "redirect:/";
+    }
+
+    @DeleteMapping("/post/{no}")
+    public String delete(@PathVariable("no") Long no) {
+        boardService.deletePost(no);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/post")
+    public String write() {
+        return "board/write.html";
+    }
+
+    @PostMapping("/post")
+    public String write(BoardDto boardDto) {
+        boardService.savePost(boardDto);
+
+        return "redirect:/";
+    }
+}
